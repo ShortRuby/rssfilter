6205abe5e2bfa0e20fd0ea808a287e9be926f13d
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2023-05-24 20:42:40 +0300

    Removed cdata as RSS escapes all tags

--- app/rss/maker.rb
+++ app/rss/maker.rb
@@ -6,10 +6,10 @@ module NewsletterRss
       AUTHOR = -"Lucian Ghinda"
       def call(channel:, items:)
         RSS::Maker.make("2.0") do |maker|
-          maker.channel.author = cdata_wrap(AUTHOR)
-          maker.channel.updated = cdata_wrap(Time.now.to_s)
-          maker.channel.link = cdata_wrap(channel.link)
-          maker.channel.webMaster = cdata_wrap(web_master(channel))
+          maker.channel.author = AUTHOR
+          maker.channel.updated = Time.now.to_s
+          maker.channel.link = channel.link
+          maker.channel.webMaster = web_master(channel)
 
           copy_attributes(
             %i(title language description copyright lastBuildDate itunes_author),
@@ -19,9 +19,9 @@ module NewsletterRss
 
           items.each do |item|
             maker.items.new_item do |new_item|
-              new_item.link = cdata_wrap(item.link)
-              new_item.title = cdata_wrap(item.title)
-              new_item.description = cdata_wrap(item.description)
+              new_item.link = item.link
+              new_item.title = item.title
+              new_item.description = item.description
             end
           end
         end
@@ -31,15 +31,13 @@ module NewsletterRss
 
         def copy_attributes(attrs, source:, destination:)
           attrs.each do |attr|
-            destination.send("#{attr}=", cdata_wrap(source.send(attr)))
+            destination.send("#{attr}=", source.send(attr))
           end
         end
 
         def web_master(channel)
           "#{channel.webMaster} (#{AUTHOR})"
         end
-
-        def cdata_wrap(text) = "<![CDATA[#{text}]]>"
 
         def logger = NewsletterRss::App["logger"]
     end
